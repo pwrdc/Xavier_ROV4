46bfc63afeb2e9f8145f0b3204c8d7dc752adae8
Bartosz Stucke <bartoszstucke@macbook-pro-user.home>
2019-05-22 13:14:00 +0200

    minor fixes

--- locator/cv_solution/cpp_code/path.cpp
+++ locator/cv_solution/cpp_code/path.cpp
@@ -137,27 +137,6 @@ void PathDetector::findLinesParameters(cv::Mat frame)
     }
     
     countLinesCoordinates(frame);
-    /*for( size_t i = 0; i < lines.size(); i++ )
-    {
-        float rho = lines[i][0], theta = lines[i][1];
-        Point pt1, pt2;
-        double a = cos(theta), b = sin(theta);
-        double x0 = a*rho, y0 = b*rho;
-        pt1.x = cvRound(x0 + 1000*(-b));
-        pt1.y = cvRound(y0 + 1000*(a));
-        pt2.x = cvRound(x0 - 1000*(-b));
-        pt2.y = cvRound(y0 - 1000*(a));
-        if (sin(theta) < 0.05)
-        {
-            cout << "Vertical :" << sin(theta) << endl;
-            line( frame, pt1, pt2, Scalar(0,0,255), 3);
-        }
-        else if (sin(theta) > 0.995)
-        {
-            cout << "Horizontal :" << sin(theta) << endl;
-            line( frame, pt1, pt2, Scalar(0,255,0), 3);
-        }
-    }*/
 }
 
 vector<vector<double>> PathDetector::sortParameters(vector<cv::Vec2f> &lines)
@@ -264,7 +243,7 @@ vector<double>PathDetector::countVerticalAverage(vector<vector<double>> &tempPar
         }
     }
     
-    tempParams[0] = M_PI - 3.14;
+    tempParams[0] = M_PI - 3.14; //way to approach problem that some nearly-vertical lines' theta ~ 3,14, others' ~0
     tempParams[1] = sumRho/counter;
     
     return tempParams;
