48213cf18160734e2f14a435c92462e4ec990122
bartkez <44206685+bartkez@users.noreply.github.com>
2019-05-24 00:00:53 +0200

    Fixed
    magic numbers as static values, method has been refactored according to guideline, imshow is now commented as there's no use of it

--- tasks/tasks/torpedoes/cv_solution/python_code/ellipse.py
+++ tasks/tasks/torpedoes/cv_solution/python_code/ellipse.py
@@ -6,27 +6,30 @@ MIN_AREA = 2000  # in order to ignore too small areas
 PI = 3.14159265358979323846
 MAX_TOL = 5  # depends on proximity, ca. 5 if ellipses are close
 THRESH = 134  # depends on light intensity: no water -> 120, simulation -> 134
+MIN_RATIO = 1.5
+MAX_RATIO = 2.5
+ELLIPSE_COLOUR = (0, 0, 255)
 
 
 class EllipseDetector:
-    def __init__(self, img):
-        self.img = img
 
-    def img_area(self):
-        img_info = self.img.shape
+    @staticmethod
+    def img_area(image):
+        img_info = image.shape
         x_img = img_info[0]
         y_img = img_info[1]
         max_area = x_img * y_img
         return max_area
 
-    def prepare_img(self):
-        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+    @staticmethod
+    def prepare_img(image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (5, 5), 0)
         ret, thresh = cv2.threshold(blur, THRESH, 255, 0)
         return thresh
 
-    def detect(self):
-        thresh = self.prepare_img()
+    def get_oval_holes_coordinates(self, image):
+        thresh = self.prepare_img(image)
         im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
         ellipse_coords = []
         for cnt in contours:
@@ -61,17 +64,16 @@ class EllipseDetector:
                     ratio = y/x
 
             if ellipse_area:
-                if (circle_area / ellipse_area < MAX_TOL) and (circle_area / self.img_area() * 100 < MAX_TOL):
-                    if (ratio > 1.5) and (ratio < 2.5):
+                if (circle_area / ellipse_area < MAX_TOL) and (circle_area / self.img_area(image) < MAX_TOL / 100):
+                    if (ratio > MIN_RATIO) and (ratio < MAX_RATIO):
                         # if np.any(img[x0, y0] == 0):
                         ellipse = cv2.fitEllipse(cnt)
-                        cv2.ellipse(self.img, ellipse, (0, 0, 255), 2)
+                        cv2.ellipse(image, ellipse, ELLIPSE_COLOUR, 2)
                         # print(x*y)
                         # print(("x{} y{}").format(x, y))
                         ellipse_coords.append(dict({"x": ((x0-x1)/2+x1), "y": (y1-y2)/2+y2}))
-        cv2.imshow("Keypoints", self.img)
+        # cv2.imshow("Key points", image)
         return ellipse_coords
-
 
 
 
