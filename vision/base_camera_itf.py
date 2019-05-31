5ca527fbf3eea65c7c189a5fb0c721a8f38c8563
DominikWasiolka <dominowasy@gmail.com>
2019-05-31 09:41:31 +0200

    update cameras interfaces

---
+++ vision/base_camera_itf.py
@@ -0,0 +1,13 @@
+import abc
+
+class IBaseCamera:
+    """
+    interface for all camera classes
+    """
+
+    @abc.abstractmethod
+    def get_image(self):
+        '''
+        :return: the latest image captured from camera, standard openCV type
+        '''
+        pass
