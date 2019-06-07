6d3059378ad8dad08eaa6bff6c46a354348292ce
Konrad3612 <45296896+Konrad3612@users.noreply.github.com>
2019-06-07 20:00:50 +0200

    Add files via upload

--- tests/image_detectors/crucifix_tests.py
+++ tests/image_detectors/crucifix_tests.py
@@ -3,9 +3,9 @@ import os
 import json
 from unittest import TestCase, TestSuite, TextTestRunner, makeSuite
 
-from tasks.path.locator.cv_solution.locator import Locator as CVLocator
+from tasks.crucifix.locator.cv_solution.locator import Locator as CVLocator
 
-PATH_DIRECOTRY = "./tests/image_detectors/path/"
+PATH_DIRECOTRY = "./tests/image_detectors/crucifix/"
 DELTA_CORDINATES = 0.05
 DELTA_ANGLE = 5
 
@@ -15,14 +15,14 @@ class TestRunner:
 
     def run(self):
         test_suite = TestSuite(tests=[
-            makeSuite(TestPathLocatorCV)#,
-            #makeSuite(TestPathLocatorML)
+            makeSuite(TestCrucifixLocatorCV)#,
+            #makeSuite(TestCrucifixLocatorML)
         ])
         return self.runner.run(test_suite)
 
-class TestPathLocatorCV(TestCase):
-    def test_get_path_cordinates(self):
-        # Test get_path_cordinates method of CV solution
+class TestCrucifixLocatorCV(TestCase):
+    def test_get_intersection_cordinates(self):
+        # Test get_crucifix_cordinates method of CV solution
         with open(PATH_DIRECOTRY + "coordinates.json", "r") as read_file:
             all_expected_coordinates = json.load(read_file)
 
@@ -32,38 +32,21 @@ class TestPathLocatorCV(TestCase):
 
                 image = cv2.imread(relative_filename)
                 locator = CVLocator()
-                cordinates = locator.get_path_cordinates(image)
+                cordinates = locator.get_intersection_cordinates(image)
                 expected_coordinates = all_expected_coordinates[filename]
 
                 self.assertAlmostEqual(expected_coordinates['x'], cordinates['x'], delta=DELTA_CORDINATES)
                 self.assertAlmostEqual(expected_coordinates['y'], cordinates['y'], delta=DELTA_CORDINATES)
 
 
-    def test_get_rotation_angle(self):
-        # Test get_rotation_angle method of CV solution
-        with open(PATH_DIRECOTRY + "angles.json", "r") as read_file:
-            all_expected_angles = json.load(read_file)
 
-        for filename in all_expected_angles:
-            image = cv2.imread("./path" + filename)
-            locator = CVLocator()
-            angle = locator.get_rotation_angle(image)
-
-            expected_angle = all_expected_angles[filename]
-
-            self.assertAlmostEqual(expected_angle, angle, delta=DELTA_ANGLE)
-
-
-class TestPathLocatorML(TestCase):
-    def test_get_path_cordinates(self):
+class TestCrucifixLocatorML(TestCase):
+    def test_get_intersection_cordinates(self):
         pass
 
-
-    def test_get_rotation_angle(self):
-        pass
 
 if __name__ == '__main__':
-    print("Path tester")
+    print("Crucifix tester")
     test_runner = TestRunner()
     results = test_runner.run()
 
---
+++ tests/image_detectors/garlic_tests.py
@@ -0,0 +1,70 @@
+import cv2
+import os
+import json
+from unittest import TestCase, TestSuite, TextTestRunner, makeSuite
+
+from tasks.garlic.locator.cv_solution.locator import Locator as CVLocator
+
+PATH_DIRECOTRY = "./tests/image_detectors/garlic/"
+DELTA_CORDINATES = 0.05
+
+class TestRunner:
+	def __init__(self):
+		self.runner = TextTestRunner(verbosity=2)
+
+	def run(self):
+		test_suite = TestSuite(tests=[
+			makeSuite(TestGarlicLocatorCV)#,
+			#makeSuite(TestGarlicLocatorML)
+		])
+		return self.runner.run(test_suite)
+
+class TestGarlicLocatorCV(TestCase):
+	def test_get_garlic_coordinates(self):
+		# Test get_garlic_coordinates method of CV solution
+		with open(PATH_DIRECOTRY + "garlic_coordinates.json", "r") as read_file:
+			all_expected_coordinates = json.load(read_file)
+
+		for filename in os.listdir(PATH_DIRECOTRY):
+			if filename.endswith("_garlic.png"):
+				relative_filename = PATH_DIRECOTRY+filename
+
+				image = cv2.imread(relative_filename)
+				locator = CVLocator()
+				cordinates = locator.get_garlic_cordinates(image)
+				expected_coordinates = all_expected_coordinates[filename]
+
+				self.assertAlmostEqual(expected_coordinates['x'], cordinates['x'], delta=DELTA_CORDINATES)
+				self.assertAlmostEqual(expected_coordinates['y'], cordinates['y'], delta=DELTA_CORDINATES)
+
+	def test_get_garlic_handle_coordinates(self):
+		# Test get_garlic_handle_coordinates method of CV solution
+		with open(PATH_DIRECOTRY + "garlic_handle_coordinates.json", "r") as read_file:
+			all_expected_coordinates = json.load(read_file)
+
+		for filename in os.listdir(PATH_DIRECOTRY):
+			if filename.endswith("_garlic_handle.png"):
+				relative_filename = PATH_DIRECOTRY+filename
+
+				image = cv2.imread(relative_filename)
+				locator = CVLocator()
+				cordinates = locator.get_garlic_handle_cordinates(image)
+				expected_coordinates = all_expected_coordinates[filename]
+
+				self.assertAlmostEqual(expected_coordinates['x'], cordinates['x'], delta=DELTA_CORDINATES)
+				self.assertAlmostEqual(expected_coordinates['y'], cordinates['y'], delta=DELTA_CORDINATES)
+
+
+class TestGarlicLocatorML(TestCase):
+	def test_get_garlic_coordinates(self):
+		pass
+	def test_get_garlic_handle_coordinates(self):
+		pass
+
+if __name__ == '__main__':
+	print("Garlic tester")
+	test_runner = TestRunner()
+	results = test_runner.run()
+
+	if results.failures:
+		print("Tests fail")
