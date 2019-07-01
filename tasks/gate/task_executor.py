244b8090bc0d00f310738f292877dd38c5d5051e
Karol Adamiak <karol.kosciany@gmail.com>
2019-07-01 19:00:23 +0200

    Update task_executor.py

--- tasks/gate/task_executor.py
+++ tasks/gate/task_executor.py
@@ -3,5 +3,5 @@ from .. import task_executor_itf
 class TaskExecutor(task_executor_itf.ITaskExecutor):
     def __init__(self):
         pass
-    def __run__(self):
+    def run(self):
         pass
