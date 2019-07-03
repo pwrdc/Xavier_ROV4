4da17a1928dc2e3581399511cdd4c49f0c5d8a9a
Bartosz Stucke <bartoszstucke@macbook-pro-user.home>
2019-07-03 16:38:36 +0200

    Solution added

---
+++ tasks/casket/locator/cv_solution/cpp_code/VampireLeverDetector.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <iostream>
+#include <map>
+
+
+class VampireLeverDetector
+{
+public:
+    VampireLeverDetector(std::string fileName);
+    
+    void run();
+    std::map<std::string, double> getLeverCoordinates(cv::Mat frame);
+    void setLowHSV(int H, int S, int V);
+    void setHighHSV(int H, int S, int V);
+    
+private:
+    int lowTreshH = 0;
+    int lowTreshS = 0;
+    int lowTreshV = 0;
+    int highTreshH = 134;
+    int highTreshS = 195;
+    int highTreshV = 255;
+	int minLineLength = 30;
+    
+    cv::Mat image;
+    //
+    // Description:
+    //  Vector containing ? parameters:
+    //
+    //  [0] -> theta1
+    //  [1] -> rho1
+    //  [2] -> theta2
+    //  [3] -> rho2
+    //  ...
+    //
+    std::vector<double> averageParameters;
+    std::vector<cv::Vec4i> findLinesParameters(cv::Mat frame);
+    
+    void normalizeCoordinates(double& x, double& y, cv::Mat frame);
+};
