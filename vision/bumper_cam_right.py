739fc62c06e819d160d168e42be36d96d0b8cbd9
DominikWasiolka <dominowasy@gmail.com>
2019-07-10 17:08:32 +0200

    add rest of cameras interfaces - empty clases

---
+++ vision/bumper_cam_right.py
@@ -0,0 +1,19 @@
+import cv2
+from vision.base_camera_itf import IBaseCamera
+
+class BumperCamRight(IBaseCamera):
+    '''
+    Camera positioned at the right bumper
+    '''
+    def __init__(self, mode="HARDWARE", simulation_ref=None):
+        """
+        :param: hardware - decide if camera get video from simulation or real camera
+            two option: "HARDWARE" or "SIMULATION"
+        """
+        pass
+
+    def get_image(self):
+        '''
+        :return: the latest image captured from camera, standard openCV type
+        '''
+        pass
