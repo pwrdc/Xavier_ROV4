36b1b39d9dd294db5ec00fc61dbb1de8a03a8b70
Korczak <Szymonkor8@gmail.com>
2019-07-10 19:55:21 +0200

    updated definitions of all cameras

--- vision/bumper_cam_right.py
+++ vision/bumper_cam_right.py
@@ -10,10 +10,29 @@ class BumperCamRight(IBaseCamera):
         :param: hardware - decide if camera get video from simulation or real camera
             two option: "HARDWARE" or "SIMULATION"
         """
-        pass
+        self.mode = mode
+        self.simulation_ref = simulation_ref
+        self.get_img_ref = self.get_hardware_image
+        if mode == 'SIMULATION':
+            self.get_img_ref = self.get_simulation_image
+        elif mode == 'HARDWARE':
+            self.cap = cv2.VideoCapture(CAMERAS.BUMBER_CAMERA_RIGHT_NR)
+            self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1);
+        elif mode == "ROV3":
+            # use xiaomi wifi camera
+            #TODO
+            pass
 
     def get_image(self):
         '''
         :return: the latest image captured from camera, standard openCV type
         '''
-        pass
+        return self.get_img_ref()
+
+    def get_simulation_image(self):
+        self.simulation_ref.set_camera_focus(0)
+        return self.simulation_ref.get_image()
+
+    def get_hardware_image(self):
+        _, frame = self.cap.read()
+        return cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
