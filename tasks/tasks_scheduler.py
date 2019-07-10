41ad271bde963159a7ff8683987fed5248e85c72
DominikWasiolka <dominowasy@gmail.com>
2019-07-10 17:59:34 +0200

    reorganize task executors reorganization

--- tasks/tasks_scheduler.py
+++ tasks/tasks_scheduler.py
@@ -6,7 +6,7 @@ from tasks.path.task_executor import TaskExecutor as PathExecutor
 
 class TaskSchedululer(ITaskExecutor):
 
-    def __init__(self, movement_object, cameras_dict):
+    def __init__(self, control_dict, sensors_dict, cameras_dict, main_logger):
         """
         @param: movement_object is object of Movements Class
             (repository RPi_ROV4: RPi_ROV4/blob/master/control/movements/movements_itf.py)
@@ -14,11 +14,13 @@ class TaskSchedululer(ITaskExecutor):
             keywords: arm_camera; bottom_camera; front_cam1;
             for cameras objects look at /vision/front_cam_1.py
         """
-        self.movement_object = movement_object
+        self.control_dict = control_dict
+        self.sensors_dict = sensors_dict
         self.cameras_dict = cameras_dict
+        self.logger = main_logger
 
     def run(self):
         """
         This method is started by main object.
         """
-        pass
+        self.logger.log("Task scheduler is running")
