8f7e3184ae649ee46d18ccb2f0238a1b7cd128ca
DominikWasiolka <dominowasy@gmail.com>
2019-07-10 16:50:44 +0200

    reorganize vision module - add front cam 1

--- vision/bottom_cam_def.py
+++
@@ -1,5 +0,0 @@
-"""
-Contain definitions and options for bottom camera
-"""
-CAM_RESOLUTION_HEIGHT = 480
-CAM_RESOLUTION_WIDTH = 640
--- vision/front_cam_1_def.py
+++
@@ -1,5 +0,0 @@
-"""
-Contain definitions and options for first front camera
-"""
-CAM_RESOLUTION_HEIGHT = 720
-CAM_RESOLUTION_WIDTH = 1280
