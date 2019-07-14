046dc7fa0007d1679e19ed31bc0cae31872b2414
Kacperk6 <45367174+Kacperk6@users.noreply.github.com>
2019-07-14 21:15:58 +0200

    Update holes_detector.py
    podstawowo radził sobie ze wszystkimi obrazami poza 4 - porównanie serca z obrazu do wzorca miało wynik o rząd gorszy niż dla pozostałych obrazów, gorszy niż wyniki dla niektórych elips z pozostałych obrazów, ale nadal lepszy niż dla elips z obrazu 4. Więc teraz po prostu porównuje wyniki dla wszystkich konturów z obrazu i wybiera ten o najlepszym (najniższym) wyniku.
    Poza tym to jeszcze kompletnie się gubił na wykrywaniu wewnętrznej krawędzi serca, to usunąłem

--- tasks/torpedoes/holes_detector.py
+++ tasks/torpedoes/holes_detector.py
@@ -14,21 +14,29 @@ class HolesDetector():
         template = cv2.imread(self.CURRENT_DIRECTORY + 'heart_template.png', cv2.CV_8UC1)
         self.contours_template, self.hierarchy_template = cv2.findContours(template, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
+    def draw_contours(self, contours, image, name,  hierarchy=None):
+        image_copy = np.zeros((image.shape[0], image.shape[1], 3))
+        if hierarchy is not None:
+            for i in range(0, int(hierarchy[0].size / 4)):
+                cv2.drawContours(image_copy, contours, i, (0, 0, 255), 1)
+                cv2.imshow(name, image_copy)
+        else:
+            cv2.drawContours(image_copy, contours, -1, (0, 0, 255), 1)
+            cv2.imshow(name, image_copy)
+
     def prepareImage(self, frame):
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         frame = self.thresholdImage(frame)
-        frame = self.doMorphOperations(frame)
-        #frame = self.blurrImage(frame)
         return frame
 
     def thresholdImage(self, frame):
-        lower_h = 50
-        lower_s = 100
-        lower_v = 100
+        lower_h = 60
+        lower_s = 165
+        lower_v = 127
         lower_arr = np.array([lower_h, lower_s, lower_v])
-        upper_h = 70
+        upper_h = 82
         upper_s = 255
-        upper_v = 255
+        upper_v = 165
         upper_arr = np.array([upper_h, upper_s, upper_v])
         frame = cv2.inRange(frame, lower_arr, upper_arr)
         return frame
@@ -53,27 +61,27 @@ class HolesDetector():
         contours, hierarchy = cv2.findContours(frame, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
         return contours, hierarchy
 
-    def compareImages(self, contours, hierarchy):
-        thresh = 0.05
+    def compareImages(self, contours, hierarchy, image):
         hole = np.array([])
+        comparison_all = []
         for i in range(0, int(hierarchy[0].size / 4)):
-            child = hierarchy[0][i][2]
-            if child != -1: # if contour has inner contour
-                comparison = cv2.matchShapes(self.contours_template[0], contours[i], 1, 0.0)  # outer contour
-                if comparison < thresh:
-                    comparison = cv2.matchShapes(self.contours_template[1], contours[child], 1, 0.0)  # inner contour
-                    if comparison < thresh:
-                        hole = contours[child]
-                        break
+            if hierarchy[0][i][2] != -1:  # if contour has inner contour
+                comparison_all.append(cv2.matchShapes(self.contours_template[0], contours[i], cv2.CONTOURS_MATCH_I1, 0.0)) # outer contour
+        comparison_min = 1
+        for i in range(0, len(comparison_all)):
+            if comparison_all[i] < comparison_min:
+                comparison_min = comparison_all[i]
+                hole = contours[hierarchy[0][i][2]]  # wewnętrzny kontur konturu najbliższego wzorcowi
         return hole
 
     def get_heart_cordinates(self, image):
         image = self.prepareImage(image)
         contours, hierarchy = self.findContours(image)
-        hole = self.compareImages(contours, hierarchy)
         coordinates = {}
-        if hole.size != 0:
-            coordinates = self.find_coordinates(hole, image)
+        if contours:
+            hole = self.compareImages(contours, hierarchy, image)
+            if hole.size:   # jeśli znalazł otwór
+                coordinates = self.find_coordinates(hole, image)
         return coordinates
 
     def find_coordinates(self, hole, frame):
@@ -97,6 +105,7 @@ class HolesDetector():
         x = (x - (frame.shape[1] / 2)) / (frame.shape[1] / 2)
         y = ((frame.shape[0] / 2) - y) / (frame.shape[0] / 2)
         return x, y
+
 
 if __name__ == '__main__':
     h = HolesDetector(current_directory="")
