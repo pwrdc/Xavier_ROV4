ac3eb375295d8ed5892d9e343407935bd25bb654
Michal Pogoda <michalpogoda@hotmail.com>
2019-07-15 23:41:26 +0200

    Rebasing

--- utils/networking.py
+++ utils/networking.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import socket
 from typing import Optional
 import requests
@@ -31,3 +32,20 @@ def post_request(url: str, data: any = None) -> Optional[str]:
         return None
 
     return response.content
+=======
+import socket
+
+
+def get_free_port() -> int:
+    """
+    Determines a free port using sockets.
+    src: https://stackoverflow.com/questions/44875422/how-to-pick-a-free-port-for-a-subprocess
+    """
+    free_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    free_socket.bind(('0.0.0.0', 0))
+    free_socket.listen(5)
+    port = free_socket.getsockname()[1]
+    free_socket.close()
+
+    return port
+>>>>>>> Removing PyCharm related files from tracking on git
