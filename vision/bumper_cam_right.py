a1b951545dafb74f4b974eb19e704862ba343da9
DominikWasiolka <dominowasy@gmail.com>
2019-07-20 16:16:55 +0200

    add __del__ to cameras

--- vision/bumper_cam_right.py
+++ vision/bumper_cam_right.py
@@ -7,21 +7,25 @@ class BumperCamRight(IBaseCamera):
     '''
     Camera positioned at the right bumper
     '''
-    def __init__(self, mode="HARDWARE", simulation_ref=None):
+    def __init__(self, mode="ROV4", simulation_ref=None):
         """
-        :param: hardware - decide if camera get video from simulation or real camera
-            two option: "HARDWARE" or "SIMULATION"
+        :param: ROV4 - decide if camera get video from simulation or real camera
+            two option: "ROV4" or "SIMULATION"
         """
         self.mode = mode
         self.simulation_ref = simulation_ref
         self.get_img_ref = self.get_hardware_image
         if mode == 'SIMULATION':
             self.get_img_ref = self.get_simulation_image
-        elif mode == 'HARDWARE':
+        elif mode == 'ROV4':
             self.cap = cv2.VideoCapture(CAMERAS.BUMBER_CAMERA_RIGHT_NR)
             self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
         elif mode == "ROV3":
             self.get_img_ref = self.get_xiaomi_image
+
+    def __del__(self):
+        if self.mode == 'ROV4':
+            self.cap.release()
 
     def get_image(self):
         '''
