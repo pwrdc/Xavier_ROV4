6bd686228e74515bb7feef8f59460086a40b1e9c
Michal Pogoda <michalpogoda@hotmail.com>
2019-07-20 17:52:34 +0200

    Auto model download script

---
+++ models/download_models.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+megatools dl 'https://mega.nz/#F!lX4E0C7D!W4VWZ95lwm_Xv44PDPEtVg'
--- utils/python_rest_subtask.py
+++ utils/python_rest_subtask.py
@@ -45,20 +45,28 @@ class PythonRESTSubtask(PythonSubtask):
             if timeout is not None and time.time() - start_time > timeout:
                 return False
 
-    def post(self, url, data, pickle_data=True, unpickle_result=True) -> Any:
+    def post(self, url, data, pickle_data=True, unpickle_result=True, retries=3) -> Any:
         """
         Sends a post request to REST server
         :param url: Url to rest service, without server adress. Eg: yolo/predict
         :param data: data that will be supplied with post request.
         :param pickle_data: serialize input data with pickle library if set to True
         :param unpickle_result: deserialize response with pickle library if set to True
+        :param retries: max retries before giving up
         :return: Response from REST server
         """
+        result = None
         if pickle_data:
             data = pickle.dumps(data)
+            
+        for i in range(retries):
+            try: 
+                req = requests.post(url=f"http://localhost:{self.port}/{url}", data=data)
+                result = req.content
+                break
+            except Exception:
+                time.sleep(0.1)
 
-        req = requests.post(url=f"http://localhost:{self.port}/{url}", data=data)
-        result = req.content
 
         if unpickle_result:
             result = pickle.loads(result)
